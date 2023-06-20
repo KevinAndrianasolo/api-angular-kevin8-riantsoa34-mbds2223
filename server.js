2f91c7a180997d0af2c7d22c44fa68955fd6bc40
Riantsoa Ramanampamonjy <ramanampamonjysandratra@gmail.com>
2023-06-20 09:18:14 +0300

    authentification

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ let app = express();
 let bodyParser = require('body-parser');
 let assignment = require('./routes/assignments');
 let matiere = require('./routes/matieres');
-
-
+let user = require('./routes/user');
+let middleware = require('./utils/tokenVerify');
 let mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 //mongoose.set('debug', true);
@@ -30,8 +30,9 @@ mongoose.connect(uri, options)
 
 // Pour accepter les connexions cross-domain (CORS)
 app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Credentials', true);
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,Authorization");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
 });
@@ -46,18 +47,28 @@ let port = process.env.PORT || 8010;
 const prefix = '/api';
 
 app.route(prefix + '/assignments')
-  .get(assignment.getAssignments)
-  .post(assignment.postAssignment)
-  .put(assignment.updateAssignment);
+  .get(middleware.verifyToken,assignment.getAssignments)
+  .post(middleware.verifyToken,assignment.postAssignment)
+  .put(middleware.roleAdmin,assignment.updateAssignment);
 
 app.route(prefix + '/assignments/:id')
   .get(assignment.getAssignment)
-  .delete(assignment.deleteAssignment);
+  .delete(middleware.roleAdmin,assignment.deleteAssignment);
 
 app.route(prefix + '/matieres')
   .get(matiere.getMatieresSansPagination)
   .post(matiere.postMatiere)
 
+app.route(prefix + '/auth/login')
+  .post(user.login)
+  
+app.route(prefix + '/auth/register')
+  .post(user.register)
+
+app.route(prefix + '/auth/logout')
+  .get(middleware.verifyToken,user.logout)
+app.route(prefix + '/auth/me')
+  .get(middleware.verifyToken,user.getUserConnected)
 
 // On démarre le serveur
 app.listen(port, "0.0.0.0");
